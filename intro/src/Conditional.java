3b15e9e3ea0d99d1abcc3d286df90cdc90f0a67d
Babatunde Koiki <koikibabatunde14@gmail.com>
2022-08-24 06:16:44 +0100

    conditionals

---
+++ intro/src/Conditional.java
@@ -0,0 +1,37 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println(num1 == num2);
+        System.out.println(num1 > num2);
+        System.out.println(num1 < num2);
+        System.out.println(num1 >= num2);
+        System.out.println(num1 <= num2);
+        System.out.println(num1 != num2);
+
+        if (num1 == num2) {
+            System.out.println("num1 is equal to num2");
+        } else {
+            System.out.println("num1 is not equal to num2");
+        }
+
+        int income = Integer.parseInt(System.console().readLine("Enter your income: "));
+        boolean hasHighIncome = (income > 100_000);
+        System.out.println(hasHighIncome);
+        String className = income > 100_000 ? "First"  : income > 50_000 ? "Business" : "Economy";
+
+        System.out.println(className);
+
+        switch (className) {
+            case "First":
+                System.out.println("You are in First Class");
+                break;
+            case "Economy":
+                System.out.println("You are in Economy Class");
+                break;
+            default:
+                System.out.println("You are in Business Class");
+                break;
+        }
+    }
+}
